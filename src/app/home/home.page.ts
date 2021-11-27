9cdbef72d08d063bb3d77fad400c691582185d57
RafaBrito7 <rafabrito.office@gmail.com>
2021-11-27 18:39:43 -0300

    Home Page finalizada com Roteamento para Login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(private route: Router, private http: HttpClient) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.fetchPhrase();
+  }
 
+  phrase: any = {};
+  loadingPhrase: boolean = true;
+
+  goToLogin() {
+    this.route.navigate(['/login']);
+  }
+
+  setPhrase(value) {
+    this.phrase = value;
+  }
+
+  setLoadingPhrase() {
+    setTimeout(() => {
+      this.loadingPhrase = false;
+    }, 2000);
+  }
+
+  fetchPhrase() {
+    fetch('./assets/phrases.json')
+      .then((response: any) => {
+        return response.json();
+      })
+      .then((json: any) => {
+        let phrasesList = json;
+        let phrase =
+          phrasesList[Math.floor(Math.random() * phrasesList.length)];
+        this.setPhrase(phrase);
+        this.setLoadingPhrase();
+      });
+  }
 }
