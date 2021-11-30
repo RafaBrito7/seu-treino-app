3aa4649c17e8afde210f2cfa22664ef950165320
RafaBrito7 <rafabrito.office@gmail.com>
2021-11-30 14:44:53 -0300

    Criação da Tela de DashBoard com Botões de Create e Reload. Listagem de Workouts DB

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { IonRouterOutlet, ModalController } from '@ionic/angular';
 import { User } from '../create-user/model/user-model';
 import { Workout } from '../model/workout';
+import { NewWorkoutModalPage } from '../new-workout-modal/new-workout-modal.page';
 import { UserService } from '../services/user-service.service';
 import { WorkoutsService } from '../services/workouts.service';
 
@@ -15,7 +17,9 @@ export class DashboardPage implements OnInit {
   constructor(
     private workoutService: WorkoutsService, 
     private activeRoute: ActivatedRoute,
-    private userService: UserService) { 
+    private userService: UserService,
+    private modalController: ModalController,
+    private routerOutlet: IonRouterOutlet) { 
     this.id = this.activeRoute.snapshot.paramMap.get('id');
   }
 
@@ -30,6 +34,20 @@ export class DashboardPage implements OnInit {
 
   fetchWorkouts() {
     this.workoutService.getWorkouts().subscribe(res => this.workouts = res);
+  }
+
+  reloadWorkouts(){
+    this.fetchWorkouts();
+  }
+
+  async openNewWorkoutModal() {
+    const modal = await this.modalController.create({
+      component: NewWorkoutModalPage,
+      swipeToClose: true,
+      presentingElement: this.routerOutlet.nativeEl
+    });
+  
+    return await modal.present();
   }
 
 }
